e477692d36053726b4a24f84357e13d0f73dd6ba
Eko <swdev.bali@gmail.com>
2019-12-08 14:42:59 +0700

    done dockerizing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-API_URL = 'http://localhost:8000/api/v1'
+API_URL = 'http://djangoapp:8000/api/v1'
 
 @app.route('/')
 def index():
